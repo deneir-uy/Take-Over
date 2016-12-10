039f91b363d7c6884113c5158bd15674fd83a520
deneir-uy <deneiruy@gmail.com>
2016-12-10 21:11:06 +0800

    initial commit

---
+++ Take Over/src/take/over/TakeOver.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package take.over;
+
+/**
+ *
+ * @author deneir-uy
+ */
+
+class Process {
+    
+}
+public class TakeOver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
